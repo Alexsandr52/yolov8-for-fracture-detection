f52b98e6c6212462132635048cd1068007ebd920
Alexsandr52 <ak.polyanskiy@gmail.com>
2024-04-22 00:47:58 +0300

    fix predict

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import os
-from flask import Flask, request, jsonify
+import cv2
+import numpy as np
 from ultralytics import YOLO
+from flask import Flask, request, jsonify
 
 # docker build -t yolo-flask-app .
 # docker run -d -p 8080:8080 yolo-flask-app 
 # curl -X POST -F "file=@IMG0001780.jpg" http://localhost:8080/predict
 
 model = YOLO('runs/detect/train/weights/best.pt')
-Upload = 'static/upload'
-
 app = Flask(__name__)
 
-app.config['uploadFolder'] = Upload
+# Upload = 'static/upload'
+# app.config['uploadFolder'] = Upload
 
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
 def allowed_file(filename):
@@ -20,6 +21,27 @@ def allowed_file(filename):
 @app.route('/')
 def index():
     return '''Author: Alexander Polyansky\n Title: Flask server with Yolov8\n Description: This is simple flask server with one method [post] "predict". \n Input data must be img file. Response will be json with coordinates or error message.'''
+
+# @app.route('/predict-local', methods=['POST'])
+# def predict_clear():
+#     try:
+#         if 'file' not in request.files: return jsonify({'error': 'No file provided'})
+        
+#         file = request.files['file']
+        
+#         if file.filename == '': return jsonify({'error': 'No selected file'})
+#         if not allowed_file(file.filename): return jsonify({'error': 'Invalid file type'})
+        
+#         filePath = os.path.join(app.config['uploadFolder'], file.filename)
+#         file.save(filePath)
+
+#         res = model(filePath)
+#         res = res[0].boxes.xywh.numpy()
+#         os.remove(filePath)
+
+#         return jsonify({'boxes': res.tolist()})
+#     except Exception as e:
+#         return jsonify({'error': e})
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -31,12 +53,11 @@ def predict():
         if file.filename == '': return jsonify({'error': 'No selected file'})
         if not allowed_file(file.filename): return jsonify({'error': 'Invalid file type'})
         
-        filePath = os.path.join(app.config['uploadFolder'], file.filename)
-        file.save(filePath)
-
-        res = model(filePath)
+        np_image = np.frombuffer(file.read(), np.uint8)
+        image = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
+        
+        res = model(image)
         res = res[0].boxes.xywh.numpy()
-        os.remove(filePath)
 
         return jsonify({'boxes': res.tolist()})
     except Exception as e:
